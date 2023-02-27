d42574e75f15b2b5ace84e26e44183ae485ac119
Cristian Macedo <cristian.silvadk22@gmail.com>
2023-02-27 01:37:55 -0300

    refactor: transaction type enum

--- api/src/types/transaction.types.ts
+++ api/src/types/transaction.types.ts
@@ -1,9 +1,14 @@
 import { Types } from "mongoose";
 import { Contact } from "./contact.types";
 
+export enum TransactionType {
+  IN = "in",
+  OUT = "out",
+}
+
 export interface Transaction {
   date: Date;
-  type: "in" | "out";
+  type: TransactionType;
   name: string;
   value: number;
   contact: string | Types.ObjectId | Contact;
